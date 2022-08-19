8e4035058163fac4848024ae0dbd69aa98b0b3bd
EvelynWXY <ningyuewushang@163.com>
2022-08-19 16:15:40 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>hi</div>
+  <Evelyn />
+</template>
+
+<script>
+import Evelyn from './components/Evelyn.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    //标签：组件
+    Evelyn: Evelyn
+
+  }
+}
+</script>
