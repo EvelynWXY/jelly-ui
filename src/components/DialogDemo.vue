4188ef95ffefd6ef3d5ade25acd286481fee46d2
EvelynWXY <ningyuewushang@163.com>
2022-08-22 19:40:27 +0800

    Dialog 支持 visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,12 +2,22 @@
     <div>
         <div>Dialog 示例</div>
         <h1>示例1</h1>
-        <Dialog></Dialog>
+        <Button @click="toggle">toggle</Button>
+        <Dialog :visible="x"></Dialog>
     </div>
 </template>
 <script lang="ts">
+import { ref } from 'vue';
+import Button from '../libs/Button.vue';
 import Dialog from '../libs/Dialog.vue'
 export default {
-    components: { Dialog }
+    components: { Dialog, Button },
+    setup() {
+        const x = ref(false)
+        const toggle = () => {
+            x.value = !x.value
+        }
+        return { x, toggle }
+    }
 }
 </script>
--- src/libs/Dialog.vue
+++ src/libs/Dialog.vue
@@ -1,23 +1,34 @@
 <template>
-    <div class="xingzi-dialog-overlay"></div>
-    <div class="xingzi-dialog-wrapper">
-        <div class="xingzi-dialog">
-            <header>标题 <span class="xingzi-dialog-close"></span> </header>
-            <main>
-                <p>第一行字</p>
-                <p>第二行字</p>
-            </main>
-            <footer>
-                <Button level="main">OK</Button>
-                <Button>Cancel</Button>
-            </footer>
+    <template v-if="visible">
+        <div class="xingzi-dialog-overlay"></div>
+        <div class="xingzi-dialog-wrapper">
+            <div class="xingzi-dialog">
+                <header>标题 <span class="xingzi-dialog-close"></span> </header>
+                <main>
+                    <p>第一行字</p>
+                    <p>第二行字</p>
+                </main>
+                <footer>
+                    <Button level="main">OK</Button>
+                    <Button>Cancel</Button>
+                </footer>
+            </div>
         </div>
-    </div>
+
+    </template>
+
 </template>
 <script lang="ts">
 import Button from './Button.vue';
 export default {
-    components: { Button }
+    props: {
+        visible: {
+            type: Boolean,
+            default: false
+        }
+    },
+    components: { Button },
+
 }
 </script>
 <style lang="scss">
