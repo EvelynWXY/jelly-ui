790f0de71e81ae71b2ab70a7aa5cd01c6b30c3e3
EvelynWXY <ningyuewushang@163.com>
2022-08-22 16:13:35 +0800

    支持 level 属性

--- src/libs/Button.vue
+++ src/libs/Button.vue
@@ -17,14 +17,19 @@ export default {
         size: {
             type: String,
             default: 'normal'
+        },
+        level: {
+            type: String,
+            default: 'normal'
         }
     },
     setup(props) {
-        const { theme, size } = props
+        const { theme, size, level } = props
         const classes = computed(() => {
             return {
                 [`xingzi-theme-${theme}`]: theme,
-                [`xingzi-size-${size}`]: size
+                [`xingzi-size-${size}`]: size,
+                [`xingzi-level-${level}`]: level
             }
         })
         return { classes }
@@ -36,7 +41,8 @@ export default {
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
-$blue: #0986ff;
+$blue: #40a9ff;
+$red: red;
 $radius: 4px;
 
 .xingzi-button {
@@ -53,6 +59,7 @@ $radius: 4px;
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out($color: #000000, $amount: 0.95);
+    transition: background 250ms;
 
     &+& {
         margin-left: 8px;
@@ -107,6 +114,62 @@ $radius: 4px;
         padding: 0 4px;
     }
 
+    &.xingzi-theme-button {
+        &.xingzi-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+
+            &:hover,
+            &:focus {
+                background: darken($color: $blue, $amount: 10%);
+                border-color: darken($color: $blue, $amount: 10%)
+            }
+        }
+
+        &.xingzi-level-danger {
+            background: $red;
+            color: white;
+
+            &:hover,
+            &:focus {
+                background: darken($color: $red, $amount: 10%);
+                border-color: darken($color: $red, $amount: 10%);
+            }
+        }
+    }
+
+    &.xingzi-theme-link {
+        &.xingzi-level-danger {
+            color: red;
+
+            &:hover,
+            &:focus {
+                color: darken($color: $red, $amount: 10%);
+            }
+        }
+
+    }
+
+    &.xingzi-theme-text {
+        &.xingzi-level-main {
+            color: $blue;
+
+            &:hover,
+            &:focus {
+                color: darken($color: $blue, $amount: 10%);
+            }
+        }
+
+        &.xingzi-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($color: $red, $amount: 10%);
+            }
+        }
+    }
 
 }
 </style>
