e66ad7620b9f9fb745f90205b227617ca3142363
EvelynWXY <ningyuewushang@163.com>
2022-08-22 17:09:26 +0800

    加载中 css

--- src/libs/Button.vue
+++ src/libs/Button.vue
@@ -1,6 +1,7 @@
 <template>
 
     <button class="xingzi-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="xingzi-loadingIndicator"></span>
         <slot />
     </button>
 
@@ -23,6 +24,10 @@ export default {
             default: 'normal'
         },
         disabled: {
+            type: Boolean,
+            default: false
+        },
+        loading: {
             type: Boolean,
             default: false
         }
@@ -192,6 +197,29 @@ $radius: 4px;
         &[disabled] {
             cursor: not-allowed;
             color: $grey;
+        }
+    }
+
+    >.xingzi-loadingIndicator {
+        width: 14px;
+        height: 14px;
+        display: inline-block;
+        margin-right: 4px;
+        border-radius: 8px;
+        border-color: $blue $blue $blue transparent;
+        border-style: solid;
+        border-width: 2px;
+        animation: xingzi-spin 1s infinite linear;
+
+    }
+
+    @keyframes xingzi-spin {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
         }
     }
 }
