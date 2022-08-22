699ccd88b5a2b5efb446d4e898cba48b61ecf2ed
EvelynWXY <ningyuewushang@163.com>
2022-08-22 18:51:10 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,56 +1,13 @@
 <template>
     <div>
-
-        <p> Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
+        <div>Dialog 示例</div>
+        <h1>示例1</h1>
+        <Dialog></Dialog>
     </div>
 </template>
+<script lang="ts">
+import Dialog from '../libs/Dialog.vue'
+export default {
+    components: { Dialog }
+}
+</script>
