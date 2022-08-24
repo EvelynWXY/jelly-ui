5cbc08bb92ce94da7e496167fd9f919387da6e25
EvelynWXY <ningyuewushang@163.com>
2022-08-24 15:25:04 +0800

    优化1

--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="xingzi-tabs">
         <div class="xingzi-tabs-nav" ref="container">
-            <div class="xingzi-tabs-nav-item" v-for="(t, index) in titles" :ref="el => { if (el) navItems[index] = el }"
-                @click="select(t)" :class="{ selected: t === selected }" :key="index">{{ t }}</div>
+            <div class="xingzi-tabs-nav-item" v-for="(t, index) in titles"
+                :ref="el => { if (t === selected) selectedItem = el }" @click="select(t)"
+                :class="{ selected: t === selected }" :key="index">{{ t }}</div>
             <div class="xingzi-tabs-nav-indicator" ref="indicator"></div>
         </div>
         <div class="xingzi-tabs-content">
@@ -24,16 +25,15 @@ export default {
         }
     },
     setup(props, context) {
-        const navItems = ref<HTMLDivElement[]>([]) //<> 里是 TypeScript 的参数,TypeScript 的泛型语法
+        //<> 里是 TypeScript 的参数,TypeScript 的泛型语法
+        const selectedItem = ref<HTMLDivElement>(null)
         const indicator = ref<HTMLDivElement>(null)
         const container = ref<HTMLDivElement>(null)
         const x = () => {
-            const divs = navItems.value
-            const result = divs.filter(div => div.classList.contains('selected'))[0]
-            const { width } = result.getBoundingClientRect()
+            const { width } = selectedItem.value.getBoundingClientRect()
             indicator.value.style.width = width + 'px'
             const { left: left1 } = container.value.getBoundingClientRect()
-            const { left: left2 } = result.getBoundingClientRect()
+            const { left: left2 } = selectedItem.value.getBoundingClientRect()
             const left = left2 - left1
             indicator.value.style.left = left + 'px'
         }
@@ -67,7 +67,7 @@ export default {
             titles,
             current,
             select,
-            navItems,
+            selectedItem,
             indicator,
             container
         }
