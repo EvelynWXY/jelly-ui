0ec8be706f7b8a0fac9c0b89969a80b950637a96
EvelynWXY <ningyuewushang@163.com>
2022-08-27 15:34:57 +0800

    解决build 之后 md文件加载不出来的问题

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -4,25 +4,12 @@
 </template>
 
 <script lang="ts">
-import {
-    ref
-} from 'vue'
 export default {
     props: {
-        path: {
+        content: {
             type: String,
             required: true
         }
     },
-    setup(props) {
-        const content = ref<string>(null)
-        // 通过动态的 import 得到的东西有一个默认的 default 属性
-        import(props.path).then(result => {
-            content.value = result.default
-        })
-        return {
-            content
-        }
-    }
 }
 </script>
